b51f8c35e741db7f31371001fd2b4e1ff426039a
Hamza Ar <hamzaar@hamzas-mbp>
2018-02-24 01:41:39 +0000

    Created a roadmap background with motion effects

--- CarGame/ViewController.swift
+++ CarGame/ViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //commenttt
+    
+    @IBOutlet weak var roadImages: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var imageArray: [UIImage]!
+        imageArray = [UIImage(named: "road1.png")!,
+                      UIImage(named: "road2.png")!,
+                      UIImage(named: "road3.png")!,
+                      UIImage(named: "road4.png")!,
+                      UIImage(named: "road5.png")!,
+                      UIImage(named: "road6.png")!,
+                      UIImage(named: "road7.png")!,
+                      UIImage(named: "road8.png")!,
+                      UIImage(named: "road9.png")!,
+                      UIImage(named: "road10.png")!,
+                      UIImage(named: "road11.png")!,
+                      UIImage(named: "road12.png")!,
+                      UIImage(named: "road13.png")!,
+                      UIImage(named: "road14.png")!,
+                      UIImage(named: "road15.png")!,
+                      UIImage(named: "road16.png")!,
+                      UIImage(named: "road17.png")!,
+                      UIImage(named: "road18.png")!,
+                      UIImage(named: "road19.png")!,
+                      UIImage(named: "road20.png")!]
+        
+    
+        roadImages?.image = UIImage.animatedImage(with: imageArray, duration: 1)
+    
+
     }
 
     override func didReceiveMemoryWarning() {

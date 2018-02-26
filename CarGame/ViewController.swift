258d95166d71953f511eb719ddce4e09f6e8efba
Hamza Ar <hamzaar@hamzas-macbook-pro.local>
2018-02-26 14:04:57 +0000

    One obstacle car fall to the bottom of the screen Added the frame of the main car to the collision boundary of the obstacle car The collision boundary move automatically as the main car is being dragged.

--- CarGame/ViewController.swift
+++ CarGame/ViewController.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//This protocol explains what actions this delegate will involve.
+protocol subviewDelegate {
+    func changeSomething()
+}
+
+class ViewController: UIViewController, subviewDelegate{
     
-    
+    //Images variables
     @IBOutlet weak var roadImages: UIImageView!
+    @IBOutlet weak var player: DraggedCarView!
+    var obstacleCars = UIImageView(image: nil)
+    
+
+    //Behaviour variables
+    var dynamicAnimator: UIDynamicAnimator!
+    var collisionBehavior: UICollisionBehavior!
+    var dynamicItemBehavior: UIDynamicItemBehavior!
+
+    func changeSomething() {
+        collisionBehavior.removeAllBoundaries()
+        //make the frame of the player car to the collision boundry of the obstacle car
+        collisionBehavior.addBoundary(withIdentifier: "barrier" as
+            NSCopying, for: UIBezierPath(rect: player.frame))
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        //Assign viewController.swift as the delegate for the flower image view
+        player.myDelegate = self
+        
+        //Create an array
+        //and add all the road images to it
         var imageArray: [UIImage]!
         imageArray = [UIImage(named: "road1.png")!,
                       UIImage(named: "road2.png")!,
@@ -39,17 +64,63 @@ class ViewController: UIViewController {
                       UIImage(named: "road19.png")!,
                       UIImage(named: "road20.png")!]
         
+        //animate the road images
+        roadImages?.image = UIImage.animatedImage(with: imageArray, duration: 0.5)
+        
+        //START > Behaviour code
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        
+        //add the obstacle car to the UI
+        obstacleCars.image = UIImage(named: "car1")
+        obstacleCars.frame = CGRect(x: 118, y: -25, width: 30, height: 50)
+        self.view.addSubview(obstacleCars)
     
-        roadImages?.image = UIImage.animatedImage(with: imageArray, duration: 1)
-    
-
+        //Make the obstacle car move down
+        dynamicItemBehavior = UIDynamicItemBehavior(items: [obstacleCars])
+        self.dynamicItemBehavior.addLinearVelocity(CGPoint(x: 0, y: 170), for: obstacleCars)
+        dynamicAnimator.addBehavior(dynamicItemBehavior)
+        
+        //add items to the collision behaviour
+        collisionBehavior = UICollisionBehavior(items: [obstacleCars])
+        
+        //make the frame of the player car to the collision boundry of the obstacle car
+        collisionBehavior.addBoundary(withIdentifier: "barrier" as
+            NSCopying, for: UIBezierPath(rect: player.frame))
+        
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        
+        //add the behaviour to the dynamic animator 
+        dynamicAnimator.addBehavior(collisionBehavior)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+///////////////////////////////////////////////////////////////////////////////
+////Create a new UIImageView from scratch
+//let carView = UIImageView(image: nil)
+//
+////Assign an image to the image view
+//carView.image = UIImage(named: "car1.png")
+//
+////Assign the size and position of the image view
+//carView.frame = CGRect(x:100, y: 100, width: 30, height: 50)
+//
+//self.view.addSubview(carView)
+
+//let random = Int(arc4random_uniform(UInt32(self.view.bounds.width)))
+
+
+//
+//        var obstacleCarArray: [UIImage]!
+//            obstacleCarArray = [UIImage(named: "car1.png")!,
+//                                UIImage(named: "car2.png")!,
+//                                UIImage(named: "car3.png")!,
+//                                UIImage(named: "car4.png")!,
+//                                UIImage(named: "car5.png")!,
+//                                UIImage(named: "car6.png")!]
 
